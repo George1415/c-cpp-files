89aed50e4ca4ac5a9101999127834a3a1048f2d9
George1415 <george.koumellis@gmail.com>
2023-07-18 11:56:33 +0100

    Brute force  solution O(n^2)

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,35 @@
+/*
+1.1 Is Unique
+Implement an algorithm to determine if a string has all unique characters. 
+What if you cannot use additional data structures?
+*/
+
 #include <iostream>
 using namespace std;
+
+bool isUnique(string str)
+{
+    //if at any time we encounter 2 same characters return false
+    for (int i = 0; i < str.length() - 1; i++) {
+        for (int j = i + 1; j < str.length(); j++) {
+            if (str[i] == str[j]) {
+                return false;
+            }
+        }
+    }
+    // if no duplicate character encountered return true
+    return true;
+}
+
 int main()
 {
-    cout << "Hello, World" << endl;
-    return 0;
+    string str;
+    cin >> str;
+    if (isUnique(str)) {
+        cout << "The string " << str << " has all unique characters\n";
+    }
+    else {
+        cout << "The string " << str << " has duplicated characters\n";
+    }
+
 }
