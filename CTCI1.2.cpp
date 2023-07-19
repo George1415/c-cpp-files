ee83f1cada0411087f40e5eff985395d23cca0b0
George1415 <george.koumellis@gmail.com>
2023-07-19 13:53:36 +0100

    1.2 counting characters O(n)

--- CTCI1.2.cpp
+++ CTCI1.2.cpp
@@ -3,26 +3,29 @@ Given two strings, write a method to decide if one is a permutation of the other
 */
 
 #include <iostream>
-#include <algorithm>
 using namespace std;
-// sort both strings and then compare strings
+
+const int NUM_OF_CHAR = 256;
+
 bool checkPermutation (string str1, string str2)
 {
-    // if lengths are not equal then they cannot be permutations
-    int n1 = str1.length();
-    int n2 = str2.length();
-
-    if (n1 != n2) {
+    // if strings aren't same length, they can't be permutations of eachother
+    if (str1.length() != str2.length()) {
         return false;
     }
+    
+    // create two count arrays and initialise all values to 0
+    int count[NUM_OF_CHAR] = { 0 };
 
-    // sort strings
-    sort(str1.begin(), str1.end());
-    sort(str2.begin(), str2.end());
+    
+    
+    for (int i = 0; i < str1.length(); i++) {
+        count[str1[i]]++;
+        count[str2[i]]--;
+    }
 
-    // compare
-    for (int i = 0; i < n1; i++) {
-        if (str1[i] != str2[i]) {
+    for (int i = 0; i < NUM_OF_CHAR; i++) {
+        if (count[i]) {
             return false;
         }
     }
