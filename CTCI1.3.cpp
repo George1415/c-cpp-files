eedae14db8a01d9b3496c70f747801cca6d531a9
George1415 <george.koumellis@gmail.com>
2023-07-19 22:20:25 +0100

    1.3 count + reverse O(n)

--- CTCI1.3.cpp
+++ CTCI1.3.cpp
@@ -4,20 +4,67 @@ write a method to replace all spaces in a string with "%20".
 #include <iostream>
 using namespace std;
 
-void replaceSpaces(string str) 
+// maximum length of string after modifications
+const int MAX = 1000;
+
+int URLify(char str[])
 {
-    string add = "%20";
-    for (int i = 0; i<str.length(); i++) {
-        if(str[i] == ' ') {
-            str.replace(i, 1, add);
+    // count spaces and find current length
+    int space_count = 0, i;
+    for (i = 0; str[i]; i++)
+        if (str[i] == ' ')
+            space_count++;
+ 
+    // Remove trailing spaces
+    while (str[i-1] == ' ')
+    {
+       space_count--;
+       i--;
+    }
+ 
+    // Find new length.
+    int new_length = i + space_count * 2 + 1;
+ 
+    // New length must be smaller than length
+    // of string provided.
+    if (new_length > MAX)
+        return -1;
+ 
+    // Start filling character from end
+    int index = new_length - 1;
+ 
+    // Fill string termination.
+    str[index--] = '\0';
+ 
+    // Fill rest of the string from end
+    for (int j=i-1; j>=0; j--)
+    {
+        // inserts %20 in place of space
+        if (str[j] == ' ')
+        {
+            str[index] = '0';
+            str[index - 1] = '2';
+            str[index - 2] = '%';
+            index = index - 3;
+        }
+        else
+        {
+            str[index] = str[j];
+            index--;
         }
     }
-    cout << str;
+ 
+    return new_length;
 }
-
+ 
+// Driver code
 int main()
 {
-    string str = "Mr John Smith    ";
-    replaceSpaces(str);
+    char str[MAX] = "Mr John Smith   ";
+ 
+    // Prints the replaced string
+    int new_length = URLify(str);
+    for (int i=0; i<new_length; i++)
+        printf("%c", str[i]);
     return 0;
 }
