1e34180d9b8ca87f50568996d0bb3af3a4e425e7
George1415 <george.koumellis@gmail.com>
2023-07-20 12:10:23 +0100

    1.4 count array O(n)

---
+++ CTCI1.4.cpp
@@ -0,0 +1,39 @@
+/*
+Given a string, write a function to check if it is a permutation of a palindrome.
+*/
+#include <iostream>
+using namespace std;
+#define NO_OF_CHARS 256
+
+bool PalindromePermutation (string str)
+{
+    // create a count array and initialise all values as 0
+    int count[NO_OF_CHARS] = { 0 };
+
+    // for each count in string, increment count in the array
+    for (int i = 0; str[i]; i++) {
+        count[str[i]]++;
+    }
+    // count odd occurences
+    int odd = 0;
+    for (int i = 0; i < NO_OF_CHARS; i++) {
+        if (count[i] & 1) {
+            odd++;
+        }
+        if (odd > 1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int main() 
+{
+    string str;
+    cin >> str;
+    PalindromePermutation(str)
+    ? cout << str << " is a permutation of a palindrome\n"
+    : cout << str << " is not a permutation of a palindrome\n";
+}
+
